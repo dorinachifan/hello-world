daef7b76b94eaa91ba74ed51d59fec8f5894e5ec
dorinachifan <dorinachifan@users.noreply.github.com>
2017-02-02 11:56:51 -0500

    some silly changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,8 @@ package test1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("testing...");
+		String info = "First";
+		System.out.println("testing..." + info);
 	}
 
 }
