8d1d0091b5c2531a4dccc99b3648c1df2d44f90c
Dorina Chifan <dorina.chifan@ca.ibm.com>
2017-02-02 13:14:42 -0500

    xxx

--- src/test1/AnotherObj.java
+++ src/test1/AnotherObj.java
@@ -3,7 +3,10 @@ package test1;
 public class AnotherObj {
 	public AnotherObj(){}
 	public void runtest(){}
-	public void setTest1(String str) {
+	public void setTest1(String str, int aaa) {
+		//...
+	}
+	public void setTest22(String str) {
 		//...
 	}
 }
